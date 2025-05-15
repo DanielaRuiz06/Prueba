bed79c6d20cb9a6c4c5c5c9711aacff416a7d4b9
EdwardR12 <edward.rocha@uptc.edu.co>
2025-05-15 17:37:47 -0500

    Edward

--- Main.java
+++ Main.java
@@ -8,6 +8,8 @@ public class Main {
         int num = 2;
     System.out.println("AHHHHHHHHHHHHHHHHHHHH");
     System.out.println(num);
+    String nombre2 = "Edward";
+    System.out.println(nombre2);
     }
 
 }
