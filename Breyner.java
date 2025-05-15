a4d6ad5284e08bb31222cb00bb20ac0e350307a2
Breinet2k <drivebreyner@gmail.com>
2025-05-15 16:34:33 -0500

    Cambio hecho por Breyner

---
+++ Breyner.java
@@ -0,0 +1,6 @@
+package Prueba;
+
+public class Breyner {
+    public String nombre = "Breyner";
+    
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 package Prueba;
 
 public class Main {
-public static void main(String[] args) {
-    System.out.println("hola");
-}
+    public static void main(String[] args) {
+        System.out.println("hola");
+        int num=7;
+        System.out.println(num);
+    }
 }
