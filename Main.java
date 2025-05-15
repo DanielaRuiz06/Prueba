0c95ed31b718f8ea9fae1e8c11fc5ae38e16a698
Breinet2k <drivebreyner@gmail.com>
2025-05-15 17:10:15 -0500

    AHHHH

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ package Prueba;
 public class Main {
 public static void main(String[] args) {
     System.out.println("hola");
+    int num = 2;
+    System.out.println("AHHHHHHHHHHHHHHHHHHHH");
+    System.out.println(num);
 }
 }
