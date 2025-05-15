6a250b733dd019d0008915bd887b4547f4ade5c2
DanielaRuiz06 <paula.ruiz06@uptc.edu.co>
2025-05-15 17:50:47 -0500

    Cambio paula

---
+++ prueba/Main.java
@@ -0,0 +1,13 @@
+package Prueba;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hola");
+        String nombre = "Juan";
+        System.out.println("Hola " + nombre);
+        int num = 2;
+    System.out.println("AHHHHHHHHHHHHHHHHHHHH");
+    System.out.println(num);
+    }
+
+}
