bdb1ba63b379615bbde4db47973ad192b07ef382
Julian-M7 <skillsmaster56@gmail.com>
2025-05-15 16:34:49 -0500

    Crear un nombre

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 package Prueba;
 
 public class Main {
-public static void main(String[] args) {
-    System.out.println("hola");
-}
+    public static void main(String[] args) {
+        System.out.println("hola");
+        String nombre = "Juan";
+        System.out.println("Hola " + nombre);
+    }
 }
